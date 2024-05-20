5118a0696e9163660c45cda8eba155f898ba1a62
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-05-20 17:52:09 +0545

    home components added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import "./globals.css";
-import Banner from "@/components/Banner";
+import Banner from "@/app/(home)/Banner";
 
 export default function Home() {
   return (
